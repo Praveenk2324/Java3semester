80739be3bcb5f384ce5192b78db965e54d79a937
Rithanya <Rithanya@DESKTOP-13AJQ4H>
2024-04-11 11:54:18 +0530

    new commit

--- Praveenjava/src/Main.java
+++ Praveenjava/src/Main.java
@@ -1,12 +1,12 @@
 public class Main{
-	 public static void main(String[] args) {
-		
-		 
-		 MyThread thread1=new MyThread();
-		 MyRunnable runnable1=new MyRunnable();
-		 Thread thread2=new Thread(runnable1);
-		 
-		 
-		 
+	public static void main(String[] args) {
+		try {
+			performOperation();
+		}catch(IllegalAccessException e) {
+			System.out.println("caught illegalexception "+e.getMessage());
+		}
  }
+	public static void performOperation() throws IllegalAccessException{
+		throw new  IllegalAccessException("\nAccess Denied");
+	}
 }
